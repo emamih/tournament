f6f0628bfe50fa292ee0d7e7f9f86ad853b17138
Moritz <moritz.liepe@moejo.net>
2015-02-09 16:26:42 +0100

    Annotations hinzugefügt
    Warnings bereinigt

--- tournament-model/src/de/htw/tournament/model/Tournament.java
+++ tournament-model/src/de/htw/tournament/model/Tournament.java
@@ -2,26 +2,19 @@ package de.htw.tournament.model;
 
 import java.util.Collections;
 import java.util.Set;
-import java.util.concurrent.ThreadLocalRandom;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Index;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
